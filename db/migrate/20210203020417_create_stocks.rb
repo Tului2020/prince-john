a4ced721273c948137e5f9952ece7dfeb106e914
Tului <tgantulga@ucdavis.edu>
2021-02-03 11:51:30 -0800

    created and migrated database

--- db/migrate/20210203020417_create_stocks.rb
+++ db/migrate/20210203020417_create_stocks.rb
@@ -1,8 +1,10 @@
 class CreateStocks < ActiveRecord::Migration[5.2]
   def change
     create_table :stocks do |t|
-
+      t.string :ticker, null: false
+      t.string :company, null: false
       t.timestamps
     end
+    add_index :stocks, :ticker
   end
 end
--- db/migrate/20210203020424_create_user_stocks.rb
+++ db/migrate/20210203020424_create_user_stocks.rb
@@ -1,8 +1,11 @@
 class CreateUserStocks < ActiveRecord::Migration[5.2]
   def change
     create_table :user_stocks do |t|
-
+      t.integer :user_id, null: false
+      t.integer :stock_id, null: false
       t.timestamps
     end
+    add_index :user_stocks, :user_id
+    add_index :user_stocks, :stock_id
   end
 end
