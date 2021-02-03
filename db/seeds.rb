fa7e3831dc77b13848c2b71b9f5c097f396e033b
Tului <tgantulga@ucdavis.edu>
2021-02-03 12:32:40 -0800

    user auth finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+user1 = User.create(username: 'Tului1', password:'password', email:'tului1@gmail.com')
+user2 = User.create(username: 'Tului2', password:'password', email:'tului2@gmail.com')
+user3 = User.create(username: 'Tului3', password:'password', email:'tului3@gmail.com')
+user4 = User.create(username: 'Tului4', password:'password', email:'tului4@gmail.com')
+
+
