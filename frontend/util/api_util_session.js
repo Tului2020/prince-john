e6c82194a1f1df7416b67bda94f4097f75ec077f
Tului <tgantulga@ucdavis.edu>
2021-02-09 10:00:48 -0800

    added api utils for siging up, signing in, and signing out

---
+++ frontend/util/api_util_session.js
@@ -0,0 +1,25 @@
+import { $CombinedState } from "redux"
+
+
+export const postUser = (user) => {
+  return $.ajax({
+    url: '/api/users',
+    method: 'POST',
+    data: {user}
+  })
+}
+
+export const postSession = (user) => {
+  return $.ajax({
+    url: '/api/session',
+    method: 'POST',
+    data: {user}
+  })
+}
+
+export const deleteSession = () => {
+  return $.ajax({
+    url: '/api/session',
+    method: 'DELETE'
+  })
+}
