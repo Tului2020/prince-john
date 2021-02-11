d7d4d5050d9f16e87ef0deade6f31f99108b80c9
Tului <tgantulga@ucdavis.edu>
2021-02-10 19:04:26 -0800

    login page almost finished

--- frontend/components/sessions/session_form.jsx
+++ frontend/components/sessions/session_form.jsx
@@ -15,7 +15,10 @@ class SessionForm extends React.Component {
   }
 
   update(field) {
-    return e => this.setState({ [field]: e.currentTarget.value })
+    
+    return (e) => {
+      console.log(field, e.target.value)
+      this.setState({ [field]: e.target.value })}
   }
 
   handleSubmit(e) {
@@ -25,36 +28,49 @@ class SessionForm extends React.Component {
 
 
   render() {
-    let usernameInput = (
-      <label>Username
-        <input type="text" value={this.state.username} onChange={this.update('username')}/>
-      </label>)
-
-    let emailInput = (
-      <label>Email
-        <input type="text" value={this.state.email} onChange={this.update('email')}/>
-      </label>)
-
-    let passwordInput = (
-      <label>Password
-        <input type="password" value={this.state.password} onChange={this.update('password')}/>
-      </label>)
+    // This function simplifies different types of inputs i.e. username, password, email
+    const fieldInput = (inputDisp, inputType='text') => {
+      return (
+        <div >
+          <label id="login-page-input">
+            {inputDisp.charAt(0).toUpperCase() + inputDisp.slice(1)}
+            <br/><br/>
+            <input id="login-page-input-field" type={inputType} value={this.state[inputDisp]} onChange={this.update(inputDisp)}/>
+          </label>
+        </div>
+      )
+    }
 
     // using ternary logic to define form
     let form = (this.props.formType == 'Log In') ? 
       (
         <div className="login-page">
           <div>
-            <img id="login-picture" src="https://cdn.robinhood.com/assets/generated_assets/632fcb3e7ed928b2a960f3e003d10b44.jpg"/>
+            <img id="login-page-left" src="https://cdn.robinhood.com/assets/generated_assets/632fcb3e7ed928b2a960f3e003d10b44.jpg"/>
           </div>
-          <div>
-            <h2>Welcome to PrinceJohn</h2>
-            {usernameInput}{passwordInput}
+          <div id="login-page-right">
+
+            <div id="login-page-right-top">
+
+              <div className="welcome">
+                Welcome to PrinceJohn
+              </div>
+
+              {fieldInput('username')}
+
+              {fieldInput('password', 'password')}
+
+
+            </div>
+
+            <div>
+              <button id="login-page-login" >{this.props.formType}</button>
+            </div>
           </div>
-          <button>{this.props.formType}</button>
+          
         </div>)
       : 
-      (<div>{emailInput}{usernameInput}{passwordInput}</div>)
+      (null)
 
     
 
