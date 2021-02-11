81b972c2f032b4a61b81a24a4c165a0f132e0cdd
Tului <tgantulga@ucdavis.edu>
2021-02-11 15:18:10 -0800

    made progress on home page

--- frontend/components/home /home.jsx
+++ frontend/components/home /home.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const homeOne = (
   <div id="home-one"> 
     <div>
-      <h1>Investing for Everyone</h1>
-      <h1>Commission-free investing, plus the tools you need to put your money in motion. Sign up and get your first stock for free. Certain limitations apply.</h1>
-      
+      <h1>Investing for <br/>Everyone</h1>
+      <br/>
+      <h2>
+        Commission-free investing, plus the tools <br/>
+        you need to put your money in motion. Sign <br/>
+        up and get your first stock for free. Certain <br/>
+        limitations apply.
+      </h2>
+      <br/>
+      <div className="btn">
+        <Link id="home-signup" to="/signup">Sign Up</Link>
+      </div>
     </div>
+
     <div>
-      <img height="660" src="https://robinhoot.herokuapp.com/assets/Frontpage_image_1-478878b9c7ae69d83fe8e90f42e12d338caa5ffc18a0bc12791463a85d59e311.png" />
+      <img height="100%" src="https://robinhoot.herokuapp.com/assets/Frontpage_image_1-478878b9c7ae69d83fe8e90f42e12d338caa5ffc18a0bc12791463a85d59e311.png" />
     </div>
   
+    
+
   </div>
 
 )
 
 const homeTwo = (
   <div id="home-two">
-    <div>
-      <video id="videos" src="https://cdn.robinhood.com/assets/superbowl/superbowl.mp4"/>
-    </div>
-    <div>
-      We are all investors
+
+    {/* <div id="border"> */}
+      <video id="videos" src="https://cdn.robinhood.com/assets/superbowl/superbowl.mp4"
+        width="50%" height="59vh"
+      />
+    {/* </div> */}
+
+    <div id="home-two-style">
+      We are all investors.
     </div>
     
   </div>
@@ -48,17 +64,30 @@ Fractional Shares Disclosure</p>
 const homeFour = (
   <div id="home-four">
     <div>
-      dsaddsadasdasdasdassa
+    Get mobile access to the markets. Invest commission-free in individual companies or bundles of investments (ETFs).
     </div>
     <div>
-      <img height="540" src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/3x__e1232689b2632b308615065dcf30be41.png"/>
+      <img height="540" src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/1x__b8f3a854780a088fe18bebe63df09581.png"/>
     </div>
   </div>
 )
 
 const homeFive = (
   <div id="home-five">
-    <p>FIVE</p>
+    <div id="home-bottom">
+
+      <p>© 2021 PrinceJohn. All rights reserved.</p>
+      <br/>
+      <p>
+      PrinceJohn means PrinceJohn Markets and its web experiences.
+      </p>
+      <br/>
+      <p>
+        All investments involve risks, including the possible loss of capital.
+      </p>
+
+    </div>
+
   </div>
 )
 
@@ -83,13 +112,9 @@ class Home extends React.Component {
       <div>
         {homeOne}
         {homeTwo}
-        
-        
-        {homeThree}
-        {homeFour}
-        {homeFive}
-
-        
+        {/* {homeThree} */}
+        {/* {homeFour} */}
+        {/* {homeFive} */}
       </div>
     )
   }
