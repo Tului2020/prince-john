0ebf72cde2dd2f2669f960484bbb23e02ee965d8
Tului <tgantulga@ucdavis.edu>
2021-02-11 11:48:59 -0800

    before lunch push

--- frontend/components/home /home.jsx
+++ frontend/components/home /home.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 
 const homeOne = (
   <div id="home-one"> 
-    <p>ONE</p>
+    <div>
+      <h1>Investing for Everyone</h1>
+      <h1>Commission-free investing, plus the tools you need to put your money in motion. Sign up and get your first stock for free. Certain limitations apply.</h1>
+      
+    </div>
+    <div>
+      <img height="660" src="https://robinhoot.herokuapp.com/assets/Frontpage_image_1-478878b9c7ae69d83fe8e90f42e12d338caa5ffc18a0bc12791463a85d59e311.png" />
+    </div>
+  
   </div>
 
 )
@@ -11,23 +19,40 @@ const homeOne = (
 const homeTwo = (
   <div id="home-two">
     <div>
-      {/* <video id="videos" src="https://cdn.robinhood.com/assets/superbowl/superbowl.mp4"/> */}
+      <video id="videos" src="https://cdn.robinhood.com/assets/superbowl/superbowl.mp4"/>
     </div>
     <div>
       We are all investors
     </div>
+    
   </div>
 )
 
 const homeThree = (
   <div id="home-three">
-    <p>THREE</p>
+    <p>
+      
+Introducing Fractional Shares
+Invest in thousands of stocks with as little as $1.
+Invest Any Amount
+Choose how much you want to invest, and we’ll convert from dollars to parts of a whole share.
+Build a Balanced Portfolio
+Customize your portfolio with pieces of different companies and funds to help reduce risk.
+Trade in Real Time
+Trades placed during market hours are executed at that time, so you’ll always know the share price.
+
+Fractional Shares Disclosure</p>
   </div>
 )
 
 const homeFour = (
   <div id="home-four">
-    <p>FOUR</p>
+    <div>
+      dsaddsadasdasdasdassa
+    </div>
+    <div>
+      <img height="540" src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/3x__e1232689b2632b308615065dcf30be41.png"/>
+    </div>
   </div>
 )
 
@@ -37,11 +62,7 @@ const homeFive = (
   </div>
 )
 
-const homeSix = (
-  <div id="home-six">
-    <p>SIX</p>
-  </div>
-)
+
 
 
 
@@ -67,7 +88,7 @@ class Home extends React.Component {
         {homeThree}
         {homeFour}
         {homeFive}
-        {homeSix}
+
         
       </div>
     )
