190fb1aff33466a723e5ffe4f048d74dd5bb91a4
Tului <tgantulga@ucdavis.edu>
2021-02-12 16:28:51 -0800

    got 404 page working

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,13 +5,16 @@ import SignupFormContainer from './sessions/signup_form_container';
 import {AuthRoute} from '../util/route_util'
 import NavBarContainer from './nav_bar/nav_bar_container'
 import Home from './home/home';
+import NotFound from './errors/404';
 
 const App = () => (
   <div>
+    
     <Route exact path="/" component={NavBarContainer} />
     <AuthRoute exact path="/" component={Home} />
     <AuthRoute exact path="/login" component={LoginFormContainer} />
     <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    <Route component={NotFound} />
   </div>
 );
 
