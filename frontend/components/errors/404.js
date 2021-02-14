ba2f75471d959f22d8a247708cd560547b7a2f18
Tului <tgantulga@ucdavis.edu>
2021-02-14 15:13:54 -0800

    got 404 page working

--- frontend/components/errors/404.js
+++ frontend/components/errors/404.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => (
   <div className="not-found">
-    <h1>404 - Not Found!</h1>
+    <h1>This page is not found</h1>
     <Link to="/">
-      Go Home
+      Click Here to Go Home
     </Link>
   </div>
 );
