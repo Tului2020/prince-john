3708ecfa49c38dc9d28e86f185d1bcd3f3219d10
Tului <tgantulga@ucdavis.edu>
2021-02-15 13:52:20 -0800

    rerouting 404 pages to /error

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormContainer from './sessions/login_form_container';
 import SignupFormContainer from './sessions/signup_form_container';
-import {AuthRoute, HomeAuthRoute} from '../util/route_util'
+import {AuthRoute, HomeAuthRoute, ErrorRoute} from '../util/route_util'
 import NotFound from './errors/404';
 import HomeLoggedOut from './home/logged_out/homeloggedout';
 import HomeLoggedIn from './home/logged_in/homeloggedin';
@@ -18,7 +18,9 @@ const App = () => (
       <HomeAuthRoute exact path='/' component={HomeLoggedOut} otherComponent={HomeLoggedIn}/>
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-      <Route component={NotFound} />
+      <Route exact path='/error' component={NotFound}/>
+      <ErrorRoute />
+      
     </Switch>
   </div>
 );
--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -2,27 +2,24 @@ import React from 'react';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+
+const mSTP = state => ({
+  loggedIn: Boolean(state.session.currentUserId)
+})
+
+
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   // debugger
   <Route 
     path={path}
     exact={exact}
     render={props => 
-      !loggedIn ? <Component {...props} /> : <Redirect to='/' />
+      !loggedIn ? <Component {...props} /> : <Redirect to='/error' />
     }
   />
 )
 
-const mSTP = state => ({
-  loggedIn: Boolean(state.session.currentUserId)
-})
 
-export const AuthRoute = withRouter(
-  connect(
-    mSTP,
-    null
-  )(Auth)
-)
 
 
 const HomeAuth = ({ component: Component, otherComponent: OtherComponent, path, loggedIn, exact }) => (
@@ -35,9 +32,37 @@ const HomeAuth = ({ component: Component, otherComponent: OtherComponent, path, 
   />
 )
 
+
+const Error = ({ path, exact }) => (
+  <Route 
+    path={path}
+    exact={exact}
+    render={() => 
+      <Redirect to='/error' />
+    }
+  />
+)
+
+export const ErrorRoute = withRouter(
+  connect(
+    mSTP,
+    null
+  )(Error)
+)
+
+
+
+
 export const HomeAuthRoute = withRouter(
   connect(
     mSTP,
     null
   )(HomeAuth)
+)
+
+export const AuthRoute = withRouter(
+  connect(
+    mSTP,
+    null
+  )(Auth)
 )
