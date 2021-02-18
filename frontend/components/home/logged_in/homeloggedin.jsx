a59824f67f857c8ab8db3da0f3c94cc433fffd43
Tului <tgantulga@ucdavis.edu>
2021-02-18 14:14:54 -0800

    able to get stocks information after load, need to flatten state

--- frontend/components/home/logged_in/homeloggedin.jsx
+++ frontend/components/home/logged_in/homeloggedin.jsx
@@ -6,12 +6,15 @@ import StockBar from '../../stock_bar/stock_bar';
 
 
 class HomeLoggedIn extends React.Component {
-  UNSAFE_componentWillMount() {
+  componentDidMount() {
+    // debugger
     // NEED TO CONFIRM WITH LINA IF THERES ANOTHER WAY TO GET CURRENT USER ID
-    this.props.fetchUserInfo(parseInt(Object.keys(getState().entities.users)[0]));
+    this.userId = this.props.currentUser.id
+    this.props.fetchUserInfo(this.userId);
   }
 
   render() {
+    // debugger
     return (
     <div id="logged-in-home">
       <LoggedInNavBarContainer/>
@@ -21,7 +24,7 @@ class HomeLoggedIn extends React.Component {
         </div>
           
         <div id="logged-in-right">
-          <StockBar/>
+          <StockBar current_stocks={this.props.currentUser.current_stocks}/>
         </div>
 
       </div>
@@ -30,9 +33,13 @@ class HomeLoggedIn extends React.Component {
   }
 } 
 
+const mSTP = state => ({
+  currentUser: state.entities.users[state.session.currentUserId]
+}) 
+
 
 const mDTP = (dispatch) => ({
   fetchUserInfo: (userId) => dispatch(fetchUserInfo(userId))
 })
 
-export default connect(null, mDTP)(HomeLoggedIn);
+export default connect(mSTP, mDTP)(HomeLoggedIn);
