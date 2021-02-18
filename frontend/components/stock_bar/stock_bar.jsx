c1787cfc9a5d98287987ec53d224b935a2281d96
Tului <tgantulga@ucdavis.edu>
2021-02-18 11:31:12 -0800

    some css

---
+++ frontend/components/stock_bar/stock_bar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class StockBar extends React.Component {
+
+  render() {
+    // debugger
+    return (
+      <div>
+        {this.props.ownedStocks}
+      </div>
+    )
+  }
+
+
+}
+
+
+export default StockBar;
+
