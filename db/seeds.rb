fdfeca0ce0c4c9739249a74c0609dd198a436cd4
Tului <tgantulga@ucdavis.edu>
2021-02-17 16:00:15 -0800

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all 
+user1 = User.create(password: "123456", username: "demolicious@demo.com", first_name: 'Demo', last_name: 'Feature', balance: 10000)
+user2 = User.create(password: "123456", username: "tgantulga@ucdavis.edu", first_name: 'Tului', last_name: 'Gantulga', balance: 10000)
 
-user1 = User.create( 
-    password: "123456",
-    username: "demolicious@demo.com",
-    first_name: 'demo',
-    last_name: 'feature',
-    balance: 0
-)
+UserStock.destroy_all
+user1stock1 = UserStock.create(user_id: user1.id, ticker: 'TSLA', amount: 100, unit_price: 800)
+user1stock2 = UserStock.create(user_id: user1.id, ticker: 'QYLD', amount: 100, unit_price: 23)
+user1stock3 = UserStock.create(user_id: user1.id, ticker: 'OZON', amount: 100, unit_price: 60)
+user2stock1 = UserStock.create(user_id: user2.id, ticker: 'TSLA', amount: 100, unit_price: 350)
