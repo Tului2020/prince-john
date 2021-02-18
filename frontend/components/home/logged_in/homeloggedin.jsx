cae3c5b65611c0a9d3015da1733b1c110cc6c8d3
Tului <tgantulga@ucdavis.edu>
2021-02-17 16:15:55 -0800

    user able to retain info after refresh, need to figure out how to get userId variable

--- frontend/components/home/logged_in/homeloggedin.jsx
+++ frontend/components/home/logged_in/homeloggedin.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import LoggedInNavBarContainer from '../../nav_bar/logged_in/nav_bar_logged_in_container'
+import { fetchUserInfo } from '../../../actions/session_actions';
+import { connect } from 'react-redux';
+
 
 class HomeLoggedIn extends React.Component {
-  
+  componentWillMount() {
+    this.props.fetchUserInfo(4);
+  }
 
   render() {
     return (
@@ -13,4 +18,9 @@ class HomeLoggedIn extends React.Component {
   }
 } 
 
-export default HomeLoggedIn;
+
+const mDTP = (dispatch) => ({
+  fetchUserInfo: (userId) => dispatch(fetchUserInfo(userId))
+})
+
+export default connect(null, mDTP)(HomeLoggedIn);
