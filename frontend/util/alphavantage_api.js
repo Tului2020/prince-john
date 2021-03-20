d5409f496e6f010867d4475135c8ae16649dcd14
Tului <tgantulga@ucdavis.edu>
2021-03-20 13:44:59 -0700

    added 3 more keys

--- frontend/util/alphavantage_api.js
+++ frontend/util/alphavantage_api.js
@@ -1,18 +1,29 @@
 const axios = require('axios')
+const apiKeys = {
+	key1: 'PMC6363GDICNKO59',
+	key2: 'OQFH5RW80HB6ZGYT',
+	key3: 'YP9AGVNZ920LSPDV',
+	key4: 'YP9AGVNZ920LSPDV'}
 
-const getIntraDay = (symbol) => (callBackFunc) => {
+
+
+const getIntraDay = (symbol, keyNum=1) => (callBackFunc) => {
+	if (keyNum > Object.keys(apiKeys).length) return
 	return axios.get('https://www.alphavantage.co/query',
 		{
 			params: {
 				function: 'TIME_SERIES_INTRADAY',
 				symbol,
 				interval: '5min',
-				apikey: 'PMC6363GDICNKO59',
+				apikey: apiKeys[`key${keyNum}`],
 				datatype: 'csv'
 			}
 		})
 		.then(({ data }) => {
 			callBackFunc(data)
+		})
+		.catch(err => {
+			console.log('err')
 		})
 }
 
