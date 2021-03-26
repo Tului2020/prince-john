d3eac1c51ea5f7349ec371eb14b3f01f895b3cf6
Tului <tgantulga@ucdavis.edu>
2021-03-26 16:24:01 -0700

    able to grab and display news

--- frontend/components/stock_show/components/news.jsx
+++ frontend/components/stock_show/components/news.jsx
@@ -8,25 +8,44 @@ import { getStockNews } from '../../../util/polygon_api';
 class News extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {data: []}
+    this.processNews = this.processNews.bind(this)
     getStockNews(this.props.ticker, this.processNews)
   }
 
   render() {
+    // debugger
     return (
       <div id='news-div'>
         <div className='stock-show-title bottom-border'>News</div>
+        {this.state.data.map(news => this.displayNewsArticle(news))}
       </div>
     )
   }
 
   processNews(data) {
-    debugger
+    this.setState({data})
   }
 
 
 
-  displayNewsArticle(news) {
+  displayNewsArticle({timestamp, source, summary, title, url, image}) {
+    // debugger
+    return (
+      <div className='news'>
+        <div className='news-left'>
+          <div className='news-source-time'>{source}{timestamp}</div>
+          <div className='news-title'>{title}</div>
+          <div className='news-summary'>{summary}</div>
+        </div>
 
+        <div className='news-right'>
+          <div className='news-image'>
+            <img src={image} alt={title}/>
+          </div>
+        </div>
+      </div>
+    )
   }
 
 
