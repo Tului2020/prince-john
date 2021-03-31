55919f96a1ace30127d84d37c41f943f0f1d8a01
Tului <tgantulga@ucdavis.edu>
2021-03-30 17:08:38 -0700

    backend api to update history table is working

--- app/controllers/api/histories_controller.rb
+++ app/controllers/api/histories_controller.rb
@@ -1,7 +1,31 @@
 class Api::HistoriesController < ApplicationController
   def show 
-    # debugger
     @history = History.find_by(ticker: params[:id])
+  end
+
+  def create
+    @history = History.new(history_params)
+    if @history.save
+      render json: 'Success'
+    else
+      render json: @history.errors.full_messages, status: 401
+    end
+  end
+
+  def update
+    @history = History.find_by(ticker: params[:id])
+    if !@history.update(history_update_params)
+      render json: @history.errors.full_messages, status: 401
+    end
+  end
+
+  private
+  def history_params 
+    params.require(:history).permit(:date, :history, :ticker)
+  end
+
+  def history_update_params 
+    params.require(:history).permit(:date, :history)
   end
 
 end
